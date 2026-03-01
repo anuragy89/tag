1bb8a1d144f915e59712f1f288cec0b5652af1d2
yuva neta <anuragyadavop89@gmail.com>
2026-03-01 22:40:51 +0530

    Update tagging.py

--- handlers/tagging.py
+++ handlers/tagging.py
@@ -3,51 +3,39 @@ from pyrogram import filters
 from utils.helpers import is_admin, chunk, wait
 from utils.state import allowed
 from utils.messages import EN, HI, HINGLISH, FLIRTY, pick
-
-last_msg = {}
+from utils.ratelimit import check_rate
+from utils.errors import safe_execute
 
 def mention(u):
     return f"[{u.first_name}](tg://user?id={u.id})"
 
 async def tag_one_by_one(app, msg, users, pool):
-    cid = msg.chat.id
+    chat_id = msg.chat.id
     last = None
+
     for u in users:
-        if not allowed(cid):
+        if not allowed(chat_id):
             break
+
         text = pick(pool, last).format(m=mention(u))
         last = text
-        sent = await msg.reply(text)
-        await wait(cid)
-        await sent.delete()
+
+        sent = await safe_execute(msg.reply, text)
+        await wait(chat_id)
+        if sent:
+            await safe_execute(sent.delete)
 
 def register(app):
-
-    @app.on_message(filters.command(["admin","admins"]) & filters.group)
-    async def admins(_, m):
-        if not await is_admin(app, m.chat.id, m.from_user.id):
-            return
-        admins = [x.user async for x in app.get_chat_members(m.chat.id, filter="administrators")]
-        text = m.text.split(maxsplit=1)[1] if len(m.command) > 1 else "Admins please check"
-        await tag_one_by_one(app, m, admins, [text + " {m}"])
-
-    @app.on_message(filters.command(["all"]) & filters.group)
-    async def all_tag(_, m):
-        if not await is_admin(app, m.chat.id, m.from_user.id):
-            return
-        users = [x.user async for x in app.get_chat_members(m.chat.id)]
-        for batch in chunk(users):
-            if not allowed(m.chat.id):
-                break
-            mentions = " ".join(mention(u) for u in batch)
-            sent = await m.reply(mentions)
-            await wait(m.chat.id)
-            await sent.delete()
 
     @app.on_message(filters.command("tagall") & filters.group)
     async def tagall(_, m):
         if not await is_admin(app, m.chat.id, m.from_user.id):
             return
+
+        if not check_rate(m.chat.id, m.from_user.id):
+            await safe_execute(m.reply, "⏳ Slow down a bit 😄")
+            return
+
         users = [x.user async for x in app.get_chat_members(m.chat.id)]
         await tag_one_by_one(app, m, users, HINGLISH)
 
@@ -55,6 +43,11 @@ def register(app):
     async def entag(_, m):
         if not await is_admin(app, m.chat.id, m.from_user.id):
             return
+
+        if not check_rate(m.chat.id, m.from_user.id):
+            await safe_execute(m.reply, "⏳ Please wait before using again")
+            return
+
         users = [x.user async for x in app.get_chat_members(m.chat.id)]
         await tag_one_by_one(app, m, users, EN)
 
@@ -62,6 +55,11 @@ def register(app):
     async def hitag(_, m):
         if not await is_admin(app, m.chat.id, m.from_user.id):
             return
+
+        if not check_rate(m.chat.id, m.from_user.id):
+            await safe_execute(m.reply, "⏳ थोड़ा रुकिए 😄")
+            return
+
         users = [x.user async for x in app.get_chat_members(m.chat.id)]
         await tag_one_by_one(app, m, users, HI)
 
@@ -69,5 +67,10 @@ def register(app):
     async def jtag(_, m):
         if not await is_admin(app, m.chat.id, m.from_user.id):
             return
+
+        if not check_rate(m.chat.id, m.from_user.id):
+            await safe_execute(m.reply, "😏 Thoda patience rakho")
+            return
+
         users = [x.user async for x in app.get_chat_members(m.chat.id)]
         await tag_one_by_one(app, m, users, FLIRTY)
