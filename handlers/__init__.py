b540f14d7a61b412913b65d2c75654986d4a4415
yuva neta <anuragyadavop89@gmail.com>
2026-03-01 22:32:15 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,6 @@
-
+from .start import register as start
+from .collect import register as collect
+from .broadcast import register as broadcast
+from .tagging import register as tagging
+from .stop_resume import register as stop_resume
+from .stats import register as stats
