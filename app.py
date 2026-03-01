959b4e2f3974244eb1dbeb1cc755be93c0149fc0
yuva neta <anuragyadavop89@gmail.com>
2026-03-01 21:16:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+from config import *
+from handlers import tagging, stop_resume, start, broadcast, collect
+
+app = Client(
+    "tagbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+start.register(app)
+broadcast.register(app)
+collect.register(app)
+tagging.register(app)
+stop_resume.register(app)
+
+app.run()
