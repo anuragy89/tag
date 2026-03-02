12b07e920a820725221cc4736ce93ef3d01e5d6b
yuva neta <anuragyadavop89@gmail.com>
2026-03-02 08:59:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,18 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["tagbot"]
 
-users = db.users
-groups = db.groups
+users_col = db.users
+groups_col = db.groups
 
+
+# ---------- ADD / UPDATE ----------
 
 async def add_user(user_id: int):
-    await users.update_one(
+    await users_col.update_one(
         {"_id": user_id},
         {"$set": {"_id": user_id}},
         upsert=True
@@ -17,15 +20,32 @@ async def add_user(user_id: int):
 
 
 async def add_group(group_id: int):
-    await groups.update_one(
+    await groups_col.update_one(
         {"_id": group_id},
         {"$set": {"_id": group_id}},
         upsert=True
     )
 
 
+# ---------- REQUIRED FOR BROADCAST ----------
+
+async def get_users():
+    users = []
+    async for u in users_col.find({}):
+        users.append(u["_id"])
+    return users
+
+
+async def get_groups():
+    groups = []
+    async for g in groups_col.find({}):
+        groups.append(g["_id"])
+    return groups
+
+
+# ---------- STATS ----------
+
 async def get_stats():
-    return {
-        "users": await users.count_documents({}),
-        "groups": await groups.count_documents({})
-    }
+    users = await users_col.count_documents({})
+    groups = await groups_col.count_documents({})
+    return users, groups
