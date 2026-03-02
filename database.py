9044495283501f4c8d4c6a2bd53ab1a1d7eca2d4
yuva neta <anuragyadavop89@gmail.com>
2026-03-02 08:39:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ from config import MONGO_URI
 client = AsyncIOMotorClient(MONGO_URI)
 db = client["tagbot"]
 
-users_col = db.users
-groups_col = db.groups
+users = db.users
+groups = db.groups
 
 
 async def add_user(user_id: int):
-    await users_col.update_one(
+    await users.update_one(
         {"_id": user_id},
         {"$set": {"_id": user_id}},
         upsert=True
@@ -17,7 +17,7 @@ async def add_user(user_id: int):
 
 
 async def add_group(group_id: int):
-    await groups_col.update_one(
+    await groups.update_one(
         {"_id": group_id},
         {"$set": {"_id": group_id}},
         upsert=True
@@ -25,6 +25,7 @@ async def add_group(group_id: int):
 
 
 async def get_stats():
-    users = await users_col.count_documents({})
-    groups = await groups_col.count_documents({})
-    return users, groups
+    return {
+        "users": await users.count_documents({}),
+        "groups": await groups.count_documents({})
+    }
