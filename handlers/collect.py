1bb5c5b369644df331f6e72b5d7a4ba4d6630edd
yuva neta <anuragyadavop89@gmail.com>
2026-03-02 09:20:06 +0530

    Update collect.py

--- handlers/collect.py
+++ handlers/collect.py
@@ -1,12 +1,14 @@
 from pyrogram import filters
 from database import add_user, add_group
 
-def register(app):
+
+def collect_handler(app):
 
     @app.on_message(filters.private)
-    async def save_user(_, m):
-        await add_user(m.from_user.id)
+    async def collect_user(client, message):
+        if message.from_user:
+            add_user(message.from_user.id)
 
-    @app.on_message(filters.group)
-    async def save_group(_, m):
-        await add_group(m.chat.id)
+    @app.on_message(filters.group | filters.supergroup)
+    async def collect_group(client, message):
+        add_group(message.chat.id)
