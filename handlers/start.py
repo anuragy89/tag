a4738a75b730cb28dca86859e25099649aacb138
yuva neta <anuragyadavop89@gmail.com>
2026-03-02 08:27:43 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,25 +1,25 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.database import add_user
-from config import UPDATE_CHANNEL, BOT_USERNAME
+from config import BOT_USERNAME, UPDATE_CHANNEL
+
 
 def start_handler(app):
 
     @app.on_message(filters.private & filters.command("start"))
-    async def start_cmd(client, message):
-        user = message.from_user
-        await add_user(user.id)
+    async def start_cmd(_, message):
+        await add_user(message.from_user.id)
 
         text = (
-            "👋 **Hey Welcome!**\n\n"
-            "🤖 I am a **Smart Tagging Bot**\n"
-            "🔥 I can tag members & admins in cool styles\n\n"
-            "👇 Use buttons below to get started"
+            "👋 **Welcome!**\n\n"
+            "🤖 Smart Tagging Bot\n"
+            "🔥 Tag members & admins easily\n\n"
+            "👇 Use buttons below"
         )
 
-        buttons = InlineKeyboardMarkup([
+        keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("➕ Add Me To Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
             [InlineKeyboardButton("📢 Updates Channel", url=UPDATE_CHANNEL)]
         ])
 
-        await message.reply_text(text, reply_markup=buttons)
+        await message.reply_text(text, reply_markup=keyboard)
