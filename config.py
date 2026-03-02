c6b6db51885915dd9cd73d6c63436af6bd90274e
yuva neta <anuragyadavop89@gmail.com>
2026-03-02 09:05:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,11 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Bot info
-BOT_USERNAME = os.getenv("MentionAIbot")  # <-- ADD THIS
+BOT_USERNAME = os.getenv("MentionAIbot")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/Ace_networkop")
+
+# Owner (IMPORTANT)
+OWNER_ID = int(os.getenv("OWNER_ID"))  # <-- ADD THIS
 
 # Database
 MONGO_URI = os.getenv("MONGO_URI")
